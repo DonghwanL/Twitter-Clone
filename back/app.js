5c422bf9cff1189ff7ab40079925c7386dededfa
Donghwan Lee <ehwan007@gmail.com>
2021-05-17 22:35:00 +0900

    refactor: Router 생성 및 구조 변경

--- back/app.js
+++ back/app.js
@@ -1,9 +1,25 @@
-const http = require('http');
+const express = require('express');
+const postRouter = require('./routes/post');
+const app = express(); // 한번 호출해 준다.
 
-const server = http.createServer((req, res) => {
-  console.log(req.url, req.method);
-  res.end('Hello node');
+app.get('/', (req, res) => {
+  res.send('Hello Express!');
 });
-server.listen(3065, () => {
+
+app.get('/', (req, res) => {
+  res.send('Hello API!');
+});
+
+app.get('/posts', (req, res) => {
+  res.json([
+    { id: 1, content: 'hello' },
+    { id: 2, content: 'hello2' },
+    { id: 3, content: 'hello3' },
+  ]);
+});
+
+app.use('/post', postRouter); // Prefix로 'post'를 붙여준다.
+
+app.listen(3065, () => {
   console.log('서버 실행 중...');
 });
